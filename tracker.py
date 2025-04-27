c47ace0236cbd8518f5570edd98de77f320bdf0c
DennisNeu <76845672+DennisNeu@users.noreply.github.com>
2025-04-27 10:13:53 +0200

    Test reactive

--- tracker.py
+++ tracker.py
@@ -1,9 +1,12 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer
+from textual.widgets import Header, Footer, Static
+from textual.reactive import Reactive
 
 
 class TrackerApp(App):
     """A simple app to track time spent standing on a sit-stand desk."""
+
+    is_running = Reactive(False)
 
     CSS_PATH = "style.tcss"
 
@@ -18,6 +21,9 @@ class TrackerApp(App):
     def compose(self) -> ComposeResult:
         """Generate layout for the app."""
         yield Header(show_clock=True, icon="🕒")
+        yield Static("Time Tracker", id="title")
+        self.status_display = Static(f"I display the status {self.is_running}", id="status")
+        yield self.status_display
         yield Footer()
 
     def action_toggle_dark(self) -> None:
@@ -28,15 +34,24 @@ class TrackerApp(App):
 
     def action_start_timer(self) -> None:
         """Start the timer."""
-        pass
+        self.is_running = True
+        # self.query_one("#status").update("Timer is running")
 
     def action_pause_timer(self) -> None:
         """Pause the timer."""
+        self.is_running = False
         pass
 
     def action_reset_timer(self) -> None:
         """Reset the timer."""
         pass
+
+    def watch_is_running(self, status: bool) -> None:
+        """Update the status display when the timer state changes."""
+        if status:
+            self.status_display.update("Timer is running")
+        else:
+            self.status_display.update("Timer is paused")
 
 if __name__ == "__main__":
     app = TrackerApp()
