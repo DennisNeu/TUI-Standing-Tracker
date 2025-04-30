d19bfc4256b1150dbd3f84eab5999fd8487cde58
DennisNeu <76845672+DennisNeu@users.noreply.github.com>
2025-04-30 22:17:35 +0200

    Refactor

--- tracker.py
+++ tracker.py
@@ -1,46 +1,10 @@
-from time import monotonic
+from timer import Timer
 
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Static, Digits
+from textual.widgets import Header, Footer, Static
 from textual.reactive import Reactive
 from textual.containers import Vertical
 import pyfiglet
-
-class Timer(Digits):
-    """A custom timer widget"""
-    time = Reactive(0.0)
-    total = Reactive(0.0)
-    start_time = Reactive(monotonic)
-
-    def on_mount(self) -> None:
-        """Event handler called when widget is added to the app."""
-        self.update_timer = self.set_interval(1 / 60, self.update_time, pause=True)
-
-    def update_time(self) -> None:
-        """Method to update time to current."""
-        self.time = self.total + (monotonic() - self.start_time)
-
-    def watch_time(self, time: float) -> None:
-        """Called when the time attribute changes."""
-        minutes, seconds = divmod(time, 60)
-        hours, minutes = divmod(minutes, 60)
-        self.update(f"{hours:02,.0f}:{minutes:02.0f}:{seconds:05.2f}")
-
-    def start(self) -> None:
-        """Method to start (or resume) time updating."""
-        self.start_time = monotonic()
-        self.update_timer.resume()
-
-    def stop(self) -> None:
-        """Method to stop the time display updating."""
-        self.update_timer.pause()
-        self.total += monotonic() - self.start_time
-        self.time = self.total
-
-    def reset(self) -> None:
-        """Method to reset the time display to zero."""
-        self.total = 0
-        self.time = 0
 
 
 
@@ -48,7 +12,6 @@ class TrackerApp(App):
     """A simple app to track time spent standing on a sit-stand desk."""
 
     is_running = Reactive(False)
-    start_time = Reactive(monotonic)
 
     CSS_PATH = "style.tcss"
 
@@ -91,15 +54,6 @@ class TrackerApp(App):
         else:
             self.timer.start()
             self.is_running = True
-
-    def start_time(self) -> None:
-        """Start the timer."""
-        self.start_time = monotonic()
-        self.update_timer()
-
-    def update_time(self) -> None:
-        """Method to update time to current."""
-        self.time = self.total + (monotonic() - self.start_time) 
 
     def action_reset_timer(self) -> None:
         """Reset the timer."""
