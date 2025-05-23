7b8f53d0c8c9f36d9acfa18923e6d3af2497e769
Dennis <76845672+DennisNeu@users.noreply.github.com>
2025-05-23 05:59:08 +0200

    Work on history screen

--- highscore_screen.py
+++ highscore_screen.py
@@ -2,20 +2,34 @@
 
 from textual.screen import Screen
 from textual.app import ComposeResult
-from textual.widgets import Static
+from textual.widgets import Static, Header, Footer
+from textual.containers import Container
 
 
 class HighscoreScreen(Screen):
     """A screen to display the history of scores."""
 
+    CSS_PATH = "style.tcss"
+
+    BINDINGS = [
+        ("q", "app.pop_screen()", "Quit"),
+        ("h", "app.pop_screen()", "Back to main screen"),
+    ]
+
     def __init__(self, data_manager) -> None:
-        super().__init__()
         """Initialize the HighscoreScreen with a DataManager instance."""
+        super().__init__()
         self.data_manager = data_manager
         # self.highscores = self.data_manager.highscores # Not sure if this is correct
 
     def compose(self) -> ComposeResult:
-        yield Static("This is a test")
+        yield Container(
+            Header(show_clock=True, icon=""),
+            Static("Highscores", id="highscore_title"),
+            Static("Date: Score", id="highscore_header"),
+            Footer(),
+            id="app-wrapper",
+        )
 
 
     # This is probably bogus too
